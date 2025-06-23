51adbcd4d8108f5a0cdac4de222c0dbfb84d1a8f
景蓝Yurik <imjinglan@126.com>
2025-06-24 07:59:16 +0800

    edit router

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -30,7 +30,7 @@ export async function GET(context: APIContext) {
 				title: post.data.title,
 				pubDate: post.data.published,
 				description: post.data.description || "",
-				link: `/posts/${post.slug}/`,
+				link: `/${post.slug}/`,
 				content: sanitizeHtml(parser.render(cleanedContent), {
 					allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
 				}),
