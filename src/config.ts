12e9950c8ab462ed7adc510985cb49793e2e9860
景蓝Yurik <imjinglan@126.com>
2025-07-30 10:09:01 +0800

    :sparkles: feat: Add banner

--- src/config.ts
+++ src/config.ts
@@ -13,16 +13,16 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 225, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/banner.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "空色天絵 / NEO TOKYO NOIR 06", // Credit text to be displayed
+			url: "https://www.pixiv.net/artworks/112900922", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
@@ -75,9 +75,8 @@ export const profileConfig: ProfileConfig = {
 			icon: "fa6-brands:github",
 			url: "https://github.com/imjinglan",
 		},
-
 		{
-			name: "GitHub",
+			name: "开往",
 			icon: "fa6-solid:train-subway",
 			url: "https://www.travellings.cn/go.html",
 		},
